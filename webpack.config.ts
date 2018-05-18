736c8fc3cd34e0f543f26ab6a4ccff76c7e9fa0c
rubygba <rubygba@foxmail.com>
2018-05-18 15:34:22 +0800

    init: typescript dev

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,24 +1,24 @@
 var path = require('path');
-var HtmlWebPackPlugin = require("html-webpack-plugin");
+var HtmlWebPackPlugin = require('html-webpack-plugin');
 module.exports = {
     //项目入口
-    entry: "./src/index.ts",
+    entry: './src/app.ts',
     //输出设置
     output: {
-        filename: "bundle.js",
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     //调试工具
-    devtool: "source-map",
+    devtool: 'source-map',
     //模块加载器设置
     module: {
         loaders: [
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
         ]
     },
     //调试服务
     devServer: {
-        contentBase: path.join(__dirname, "dist"),
+        contentBase: path.join(__dirname, 'dist'),
         compress: true,
         port: 7777
     },
@@ -26,7 +26,9 @@ module.exports = {
     plugins: [
         new HtmlWebPackPlugin(
             {
-                title: "threejs学习"
+                title: 'threejs学习',
+                template: path.resolve(__dirname, 'src/index.html'),
+                inject: true
             }),
     ]
 }
