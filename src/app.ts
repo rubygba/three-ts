2a7f71e938342f0ed5bad08bc2d158f40faac498
rubygba <rubygba@foxmail.com>
2018-05-18 18:08:39 +0800

    add: webgl lib

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,31 @@
-document.write('GG')
+import './webgl-utils.js'
+import './webgl-debug.js'
+import './cuon-utils.js'
+
+// 定点着色器
+var VSHADER_SOURCE = `
+  attribute vec4 a_Position;
+  void main() {
+    gl_Position = a_Position;
+    gl_PointSize = 10.0;
+  }
+  `
+var FSHADER_SOURCE = `
+  void main() {
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0)
+  }
+  `
+
+function main() {
+  var canvas = document.getElementById('webgl')
+
+  var gl = getWebGLContext(canvas, true)
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL')
+    return
+  }
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0)
+
+  gl.clear(gl.COLOR_BUFFER_BIT)
+}
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,28 +1,28 @@
-var path = require('path');
-var HtmlWebPackPlugin = require('html-webpack-plugin');
+var path = require('path')
+var HtmlWebPackPlugin = require('html-webpack-plugin')
 module.exports = {
-    //项目入口
+    // 项目入口
     entry: './src/app.ts',
-    //输出设置
+    // 输出设置
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    //调试工具
+    // 调试工具
     devtool: 'source-map',
-    //模块加载器设置
+    // 模块加载器设置
     module: {
         loaders: [
             { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
         ]
     },
-    //调试服务
+    // 调试服务
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
         port: 7777
     },
-    //插件
+    // 插件
     plugins: [
         new HtmlWebPackPlugin(
             {
